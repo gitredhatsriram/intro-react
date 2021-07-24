2f1b53b72419f71d3d935268a55b0db452b75277
gitredhatsriram <swami.enrich@gmail.com>
2021-07-24 22:59:40 +0530

    Final changes are done

--- src/App.jsx
+++ src/App.jsx
@@ -34,9 +34,9 @@ class App extends React.Component {
   }
 
   /*Write an addStudent method here*/
-  addStudent(studentName) {
-    this.state ({
-      students: this.state.students.contact(studentName)
+   addStudent(studentName) {
+    this.setState({
+      students: this.state.students.concat(studentName)
     });
   }
 
@@ -81,7 +81,7 @@ class App extends React.Component {
     }
 
     /* Uncomment lines below to render grades*/
-    /*if (this.state.buttonClicked === "grades") {
+    if (this.state.buttonClicked === "grades") {
       tabChoice = (
         <Table
           tableNames={this.state.assignments}
@@ -90,7 +90,7 @@ class App extends React.Component {
           data={this.state.grades}
         />
       );
-    }*/
+    }
 
     return (
       <div>
